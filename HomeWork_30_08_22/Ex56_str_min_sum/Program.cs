93c9e681273bc999e0fd5f6a86163ebe7d2b732d
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-31 00:18:04 +0500

    update Readme.md

--- HomeWork_30_08_22/Ex56_str_min_sum/Program.cs
+++ HomeWork_30_08_22/Ex56_str_min_sum/Program.cs
@@ -8,62 +8,37 @@
 
 
 
-void FillArray(double[,] matr)                  //заполнение вещественными числами и печать массива
+int[] FillArraySumStr(int[,] matr)                  //заполнение и печать массива и создание массива из сумм всех строк
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
+        int[] sum_str = new int[matr.GetLength(0)];
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().NextDouble() * 20 - 10;    
-            Console.Write($"{Math.Round(matr[i, j], 1)}\t");
+            matr[i,j] = new Random().Next(1, 10);    
+            Console.Write($"{matr[i, j]}\t");
+            sum_str[i] += matr[i,j];
         }
         Console.WriteLine();
     }
+    return sum_str;
 }
-Console.Write("Введите размер матрицы. Укажите количество строк m = ");
+void MinSumStrArray(int[] array)         // нахождение номера миним.элемента в массиве из сумм строк
+{
+    int i = 1;
+    int min = 0;
+    while (i < array.Length)
+    {
+        if(array[i] < array[min])   min = i; 
+        i++;
+    }
+    Console.WriteLine($"Наименьшая сумма элементов в {i}-й строке");
+}   
+    
+Console.Write("Введите размер матрицы. Укажите количество строк, больше чем одну:  m = ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("  и столбцов n = ");
+Console.Write("  и столбцов, больше чем один:  n = ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = new double[m, n];
-FillArray(matrix);
-
-
-int[,] FillArray1(int[,] matr)                   //заполнение и печать массива
-{
-    int[,] matrix = new int[matr.GetLength(0), matr.GetLength(1)];
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matrix[i,j] = new Random().Next(-10, 10);    
-            Console.Write($"{matrix[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
-    return  matrix;
-}
-
-void SeachPosition(int[,] matr, int pos)       //поиск элемента по индексу
-{
-    int a = pos / 10;
-    int b = pos % 10;
-    if (a >= matr.GetLength(0) || b >= matr.GetLength(1))
-    {
-        Console.Write($"Такого элемента в матрице размера {matr.GetLength(0)}x{matr.GetLength(1)} нет");
-    }
-    else
-    {
-        Console.Write($"Элемент в {a + 1}-той строке и {b + 1}-том столбце равен {matr[a, b]}");
-    }
-}
-Console.WriteLine("Создаем матрицу. Количество строк m и столбцов n будут случайными, но не более 10");
-int m1 = Convert.ToInt32(new Random().Next(1, 10));
-int n1 = Convert.ToInt32(new Random().Next(1, 10));
-int[,] array = new int[m1, n1];
-int[,] matrix1 = FillArray1(array);
-Console.WriteLine();
-Console.WriteLine("Какой элемент вы хотели бы найти? Введите индекс в формате ab, где a - номер строки,  b - номер столбца, с учетом, что нумерация идет с '0'");
-int position = Convert.ToInt32(Console.ReadLine());
-SeachPosition(matrix1, position);
-
- 
+int[,] matrix = new int[m, n];
+int[] sum_str_array = FillArraySumStr(matrix);
+MinSumStrArray(sum_str_array);
