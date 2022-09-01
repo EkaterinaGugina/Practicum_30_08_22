c96ab3f9e703e2c31951f61c54f6eb9f3351b261
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-01 12:26:30 +0500

    Ex59 solved

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,62 +1,67 @@
-﻿//Ex. 
+﻿//Ex59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Наименьший элемент - 1, на выходе получим 
+//следующий массив:
+//9 4 2
+//2 2 6
+//3 4 7
 
-
-void FillArray(double[,] matr)                  //заполнение вещественными числами и печать массива
+int[,] FillArray(int[,] matr)                   //заполнение и печать массива
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().NextDouble() * 20 - 10;    
-            Console.Write($"{Math.Round(matr[i, j], 1)}\t");
+            matr[i,j] = new Random().Next(1, 10);    
+            Console.Write($"{matr[i, j]}\t");
         }
         Console.WriteLine();
     }
+    return  matr;
 }
-Console.Write("Введите размер матрицы. Укажите количество строк m = ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("  и столбцов n = ");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = new double[m, n];
-FillArray(matrix);
-
-
-int[,] FillArray1(int[,] matr)                   //заполнение и печать массива
+int[] SeachMinElemArray(int[,] array)   // поиск индекса мин эл-та матрицы и удаление этих строки и столбца
 {
-    int[,] matrix = new int[matr.GetLength(0), matr.GetLength(1)];
-    for (int i = 0; i < matr.GetLength(0); i++)
+    int[] ind_min = {0,0};
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if(array[i,j] < array[ind_min[0], ind_min[1]]) 
+                {   
+                    ind_min[0] = i;   //номер строки минимального элемента
+                    ind_min[1] = j;   //номер столбца минимального элемента
+                }
+            }
+    }
+    //Console.WriteLine($"Индекс минимального элемента: {ind_min[0]}, {ind_min[1]}");
+    return ind_min;
+}
+void PrintArray(int[,] matr, int[] position_min_element)                   //заполнение и печать массива
+{
+    for (int i = 0;  i < matr.GetLength(0); i++)
+    {
+        for (int j = 0;  j < matr.GetLength(1); j++)
         {
-            matrix[i,j] = new Random().Next(-10, 10);    
-            Console.Write($"{matrix[i, j]}\t");
+            if (i != position_min_element[0] && j != position_min_element[1])  Console.Write($"{matr[i, j]}\t");
         }
         Console.WriteLine();
     }
-    return  matrix;
 }
 
-void SeachPosition(int[,] matr, int pos)       //поиск элемента по индексу
-{
-    int a = pos / 10;
-    int b = pos % 10;
-    if (a >= matr.GetLength(0) || b >= matr.GetLength(1))
-    {
-        Console.Write($"Такого элемента в матрице размера {matr.GetLength(0)}x{matr.GetLength(1)} нет");
-    }
-    else
-    {
-        Console.Write($"Элемент в {a + 1}-той строке и {b + 1}-том столбце равен {matr[a, b]}");
-    }
-}
 Console.WriteLine("Создаем матрицу. Количество строк m и столбцов n будут случайными, но не более 10");
-int m1 = Convert.ToInt32(new Random().Next(1, 10));
-int n1 = Convert.ToInt32(new Random().Next(1, 10));
-int[,] array = new int[m1, n1];
-int[,] matrix1 = FillArray1(array);
+int m = Convert.ToInt32(new Random().Next(1, 10));
+int n = Convert.ToInt32(new Random().Next(1, 10));
+int[,] array = new int[m, n];
+int[,] matrix = FillArray(array);
 Console.WriteLine();
-Console.WriteLine("Какой элемент вы хотели бы найти? Введите индекс в формате ab, где a - номер строки,  b - номер столбца, с учетом, что нумерация идет с '0'");
-int position = Convert.ToInt32(Console.ReadLine());
-SeachPosition(matrix1, position);
+int[] ind_min_elem = SeachMinElemArray(matrix); 
+Console.WriteLine($"Индекс минимального элемента: {SeachMinElemArray(matrix)[0] + 1}, {SeachMinElemArray(matrix)[1] + 1}");
+Console.WriteLine($"Матрица с удаленными {SeachMinElemArray(matrix)[0] + 1}-й строкой и   {SeachMinElemArray(matrix)[1] + 1}-м столбцом:");
+PrintArray(matrix, ind_min_elem);
+
 
  
