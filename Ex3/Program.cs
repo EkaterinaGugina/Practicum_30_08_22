6c00b35c999f07f864aa099275d778f18f1bab3b
Ekaterina Gugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-02 19:27:03 +0500

    Ex57 solved 1

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -28,7 +28,58 @@ for (int z = min; z <= max; z++)
     }
     if (count != 0) Console.WriteLine($"'{z}' - {count} раз");
 }
+bool ValueWasUsed(int number, int[,] matrix)                // Метод, определяющий наличие элемента в массиве
+{
+    bool numExists = false;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (number == matrix[i, j])
+            {
+                numExists = true;
+                break;
+            }
+        }
 
+    }
+    if (!numExists) return false;
+    else return true;
+}
+void ShowCountOfRepetitons(int number, int[,] matrix)           // Метод, определяющий количество повторений определенного элемента в массиве
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (number == matrix[i, j]) count++;
+        }
+    }
+    Console.WriteLine($"Элемент {number} встречается {count} раз");
+}
+
+int rows = rand.Next(2, 10);
+int cols = rand.Next(2, 10);
+int[,] numbers = new int[rows, cols];
+FillMatrix(numbers);
+PrintMatrix(numbers);
+Console.WriteLine();
+
+int[,] usedValues = new int[rows, cols];                        // Массив для значений элементов, количество повторений которых мы уже посчитали
+bool valueUsed;
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        valueUsed = ValueWasUsed(numbers[i, j], usedValues);
+        if (!valueUsed)
+        {
+            ShowCountOfRepetitons(numbers[i, j], numbers);
+            usedValues[i, j] = numbers[i, j];
+        }
+    }
+}
 //второй способ
 // int m = new Random().Next(1, 10);
 // int n = new Random().Next(1, 10);
