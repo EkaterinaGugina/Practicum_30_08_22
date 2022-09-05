00ec92b5ce9a08aa4218faf548973f431e4ff1bc
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-05 13:34:19 +0500

    Ex60 solved

--- HomeWork_30_08_22/Ex60_index_elem_matrix/Program.cs
+++ HomeWork_30_08_22/Ex60_index_elem_matrix/Program.cs
@@ -1,69 +1,49 @@
 ﻿// Ex60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 //Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.//
-//Массив размером 2 x 2 x 2
-//66(0,0,0) 25(0,1,0)
-//34(1,0,0) 41(1,1,0)
-//27(0,0,1) 90(0,1,1)
-//26(1,0,1) 55(1,1,1)
 void PrintArray(int[,,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            for (int k = 0; k < matr.GetLength(2); k++)
-            {
-                Console.Write($"{matr[i, j, k]}({i}{j}{k})\t");
-            }
+            for (int k = 0; k < matr.GetLength(2); k++) Console.Write($"{matr[i, j, k]}({i}{j}{k})\t");
             Console.WriteLine();
         }
     }
 }
-int Sravn(int[,,] arr, int marker)
+int Sravn(int[,,] arr, int i, int j, int k)
 {
-    int result = -1;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    arr [i, j, k] = new Random().Next(10, 100);
+    int result = 0;
+    for (int x = 0; x <= i; x++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int y = 0; y <= j; y++)
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            for (int z = 0; z <= k; z++)
             {
-                if (arr[i, j, k] == marker)
+                if ((x == i) && (y == j) && (z == k))  break;
+                else if (arr[x, y, z] == arr[i, j, k])
                 {
-                    result = new Random().Next(10, 100);
+                    result = 1;
                     break;
                 }
             }
         }
     }
-    return result;
+    if (result == 1) arr[i, j, k] = new Random().Next(10, 100);
+    return arr[i, j, k];
 }
 int[,,] FillArrayCondition(int[,,] matr)
 {
-    int count = 1;
-    int marker = 0;
-    matr[0, 0, 0] = new Random().Next(10, 100);
-    while (count <= matr.GetLength(0) * matr.GetLength(1) * matr.GetLength(2))
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int i = 0; i < matr.GetLength(0); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            for (int j = 0; j < matr.GetLength(1); j++)
-            {
-                for (int k = 0; k < matr.GetLength(2); k++)
-                {
-
-                    matr[i, j, k] = new Random().Next(10, 100);
-                    marker = matr[i, j, k];
-                    int result = Sravn(matr, marker);
-                    if (result == -1) Console.WriteLine("Одинаковых элементов нет");
-                    else {matr[i, j, k] = result;}
-                }
-            }
+            for (int k = 0; k < matr.GetLength(2); k++) matr[i, j, k] = Sravn(matr, i, j, k);
         }
-        count++;
     }
     return matr;
 }
-int[,,] matrix = new int[4, 2, 3];
+int[,,] matrix = new int[3, 2, 3];
 int [,,] resultArray = FillArrayCondition(matrix);
 PrintArray(resultArray);
